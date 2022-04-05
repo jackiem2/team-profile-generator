eb5edf99b7e7190985906c65faf4b5bb91c98e81
jackiem2 <jacqueline.merlan@gmail.com>
2022-04-04 21:55:22 -0500

    added get() functions for github and engineer role

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,6 +2,18 @@ const Engineer = require('../lib/Engineer');
 
 test('github profile info', () => {
     const githubUser = "JackIncredible";
-    const employee = new Engineer("jack", 1, "jackincredible.com", githubUser);
+    const employee = new Engineer("jack", 1, "jackincredible@gmail.com", githubUser);
     expect(employee.github).toBe(githubUser);
+})
+
+test('get github info', () => {
+    const githubUser = "JackIncredible";
+    const employee = new Engineer("jack", 1, "jackincredible@gmail.com", githubUser);
+    expect(employee.getGithub()).toBe(githubUser);
+})
+
+test('get engineer role', () => {
+    const engineerRole = "Engineer";
+    const employee = new Engineer("jack", 1, "jackincredible@gmail.com", "JackIncredible");
+    expect(employee.getRole()).toBe(engineerRole);
 })
