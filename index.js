71830b0cbcf14e026377fc6ee1f983b9f46e8251
jackiem2 <jacqueline.merlan@gmail.com>
2022-04-11 21:54:59 -0500

    added intern and engineer prompt

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
-const Manager = require('./lib/Manager.js');
-const generateHTML = require('./src/template.js');
+const Manager = require('./lib/Manager.js'); 
+const generateTemplate = require('./src/template.js');
 
 
 employeesArray = [];
@@ -13,17 +13,17 @@ function startApp () {
         {
             type: "input",
             name: "name",
-            message: "Enter the manager's name",
+            message: "Enter the manager's name:",
         },
         {
             type: "input",
             name: "id",
-            message: "Enter manager's id",
+            message: "Enter manager's id:",
         },
         {
             type: "input",
             name:"officeNumber",
-            message: "Enter manager's office number"
+            message: "Enter manager's office number:"
         },
     ])
     .then(managerData => {
@@ -55,11 +55,71 @@ function employeePrompt () {
             case "Engineer":
                 engineerPrompt();
                 break;
+
+            
         }
     })
 }
 
+function internPrompt () {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter the intern's name:"
+        },
+        {
+            type: "input",
+            name:"id",
+            message:"Enter the intern's id:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter the intern's email:"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Enter the intern's school name:"
+        }
+    ])
+    .then(internData => {
+        const intern = new Intern(internData.name, internData.id, internData.email, internData.school)
+        employeesArray.push(intern);
+        return employeePrompt();
+    });
+}
 
+function engineerPrompt () {
+    inquirer.prompt ([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter the engineer's name:"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter the engineer's id:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter the engineer's email:"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter the engineer's GitHub username:"
+        }
+    ])
+    .then(engineerData => {
+        const engineer = new Engineer(engineerData.name, engineerData.id, engineerData.email, engineerData.github)
+        employeesArray.push(engineer)
+        return employeePrompt();
+    });
 
+}
 
-startApp()
+startApp();
